bbbba78a5b70cba51e181ab50d72c7b357d4f911
gFleka <gFleka@users.noreply.github.com>
2016-08-13 01:40:38 +0200

    Updated config.php
    Added the $url_api variable to store the url of your hosted API.

--- crawler/config.php
+++ crawler/config.php
@@ -2,6 +2,8 @@
 //Postavke
 //URL-ovi za crawlati
 
+$url_api = 'http://myideamaker.com/laravel/api';
+
 $urls = array("http://www.agenti.hr/registar-posrednika-promet-nekretninama/","http://www.agenti.hr/imenik-agenata-posredovanja-promet-nekretninama/","http://www.nekretnine-rijeka.com/agencije-za-nekretnine.php","http://www.realestatecroatia.com/hrv/listag.asp");
 //Automatski populira url sa predefiniranih lokacija
 include_once("populateConfigRealEstateCroatia.php");
